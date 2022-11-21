400289a3a7e05625c9528be622b813ac669c374d
Bruno Cardoso <brunocardoso@sapo.cv>
2022-11-21 15:13:43 -0100

    add+

--- index.js
+++ index.js
@@ -1,30 +1,57 @@
-const express = require('express')                      //importando
-var bodyParser = require('body-parser')                 //importando
+const express = require('express')                             //importando
+var bodyParser = require('body-parser')                        //importando
  
 const app = express()
 const port = 3000
 
-app.use(bodyParser.urlencoded({ extended: false }))  //necessario para body parser funcionar corretamente
-app.use(bodyParser.json())                             //necessario para body parser funcionar corretamente
+app.use(bodyParser.urlencoded({ extended: false }))          //necessario para body parser funcionar corretamente
+app.use(bodyParser.json())                                  //necessario para body parser funcionar corretamente
 
 let listOfPersons = [
     {
+        "id": 11,
         "name": "Maria",
         "phone": 9532569
     },
     {
+        "id": 12,
         "name": "Fernanda",
         "phone": 3435343
     },
     {
+        "id": 13,
         "name": "Carla",
         "phone": 4534353
     },
     {
+        "id": 14,
         "name": "Pedro",
         "phone": 1273213
     }
 ];
+
+function generateID(){
+    const lastPerson = listOfPersons[listOfPersons.length-1];
+    return lastPerson.id++
+}
+
+
+app.get('/persons/:id', (req, res) => {
+
+     let idNumber = req.params.id
+
+     if(idNumber!=undefined){
+
+        for(item of listOfPersons){
+            if (item.id == idNumber){
+                res.send(item);
+            }
+        }
+    } else{
+          res.send(listOfPersons);
+    }
+
+})
 
 
 app.get('/persons', (req, res) => {
@@ -47,10 +74,9 @@ app.get('/persons', (req, res) => {
 })
 
 app.post('/persons', (req, res) => {
-    let name = req.body
-
-    listOfPersons.push(name)
-
+    let newPerson = req.body
+    newPerson.id = generateID()
+    listOfPersons.push(newPerson)
     res.send(listOfPersons)
 })
 
